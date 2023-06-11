023b6a4b7212c1cfbc0c82637782f1bcc7783617
Jack Boettcher <jcboettcher@college.harvard.edu>
2023-06-10 20:52:51 -0500

    lines

--- fyvm.js
+++ fyvm.js
@@ -3,7 +3,7 @@ let w = 0;
 let h = 0;
 let x = 0;
 let y = 0;
-const pts = [];
+let pts = [];
 
 function preload() {
   mit_image = loadImage("mit.png");
@@ -20,7 +20,6 @@ function setup() {
     h = mit_image.height/mit_image.width*w;
     y = (window.innerHeight-h)/2;
   }
-  strokeWeight(8);
   stroke(0,0,255);
 }
 
@@ -28,11 +27,28 @@ const coord = (n,e) => {
   pts.push({n,e});
 }
 
+const parseList = s => {
+  const l = s.split(";");
+  pts = [];
+  for (let ne of l) {
+    const [n,e] = ne.split(",");
+    pts.push({n:parseInt(n),e:parseInt(e)});
+  }
+}
+
 function draw() {
+  background(255);
   translate(x,y);
   image(mit_image,0,0,w,h,w/1200,h/280);
   translate(w/2,h/2);
+  strokeWeight(8);
   for (let pt of pts) {
     point(pt.e/360*w,-pt.n/120*h);
+  }
+  strokeWeight(1);
+  for (let i = 0; i < pts.length-1; i++) {
+    const pt1 = pts[i];
+    const pt2 = pts[i+1];
+    line(pt1.e/360*w,-pt1.n/120*h,pt2.e/360*w,-pt2.n/120*h);
   }
 }
